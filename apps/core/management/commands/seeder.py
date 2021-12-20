9c7571cdbc3e4539e97a66302d41006a38e33404
Mua Rachmann <muarachmann@gmail.com>
2021-12-20 10:28:10 +0100

    fixed seeder

--- apps/core/management/commands/seeder.py
+++ apps/core/management/commands/seeder.py
@@ -1,28 +1,33 @@
 from django.core.management.base import BaseCommand
-from apps.account.models import Account as AccountReport
+from apps.account.models import Account as PassportAccount
+from apps.dashboard.models import PassportMail, PassportGroupTasks
+
 
 MODE_REFRESH = 'refresh'
-
 MODE_CLEAR = 'clear'
-
-# python manage.py seeder --mode=clear/refresh
+MODE_RESET = 'reset'
 
 
 class Command(BaseCommand):
-    help = "seeding database🌱"
+    help = "seeding database 🌱"
 
     def add_arguments(self, parser):
         parser.add_argument('--mode', type=str, help="Mode")
 
-    def truncate_db(self):
-        self.stdout.write('Truncating Database 🗑️ 🌱')
-        AccountReport.objects.all().delete()
-        self.stdout.write('Truncating Completed!🤩')
+    def truncate_db(self, hard=False):
+        self.stdout.write('Truncating Database 🗑️🌱')
+        if hard:
+            PassportAccount.objects.all().delete()
+        PassportMail.objects.all().delete()
+        PassportGroupTasks.objects.all().delete()
+        self.stdout.write('Truncating Completed! 🤩')
 
     def handle(self, *args, **options):
         self.stdout.write(self.help)
         if options['mode'] == MODE_CLEAR:
             self.truncate_db()
+        elif options['mode'] == MODE_RESET:
+            self.truncate_db(hard=True)
         else:
             self.truncate_db()
-            self.stdout.write('Seeding Completed!🤩')
+            self.stdout.write('Seeding Completed! 🤩')
